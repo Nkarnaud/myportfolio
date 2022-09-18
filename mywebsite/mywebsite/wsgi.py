1b1d9b9c4d1843355315ace61b47ccac2bb55ac4
Arnaud Tsombeng <arnaud.tsombeng@gmail.com>
2022-09-18 20:49:37 +0100

    Update wsgi.py

--- mywebsite/mywebsite/wsgi.py
+++ mywebsite/mywebsite/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+path = '/workspace'
+if path not in sys.path:
+    sys.path.append(path)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
 
 application = get_wsgi_application()
